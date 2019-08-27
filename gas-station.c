f3f51025029762a4486d91c21f7b6ef481d05c2e
zzc2422 <liksasdilasely@163.com>
2019-08-27 16:41:48 +0800

    初步完成未测试

--- gas-station.c
+++ gas-station.c
@@ -1,3 +1,14 @@
 int canCompleteCircuit(const int gas_a[], int no_use, const int cost_a[],
 		       int length) {
+	int rest = 0, min_rest = 0, i = 0, min_i = 0;
+	do {
+		rest += gas_a[i] - cost_a[i];
+		if (rest < min_rest) {
+			min_rest = rest;
+			min_i = i;
+		}
+		i++;
+
+	} while (i != length);
+	return rest >= 0 ? min_i : -1;
 }
