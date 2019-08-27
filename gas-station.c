d4758b783d3457ea677a3646f8a9bc9f1c54f9b1
zzc2422 <liksasdilasely@163.com>
2019-08-27 17:57:36 +0800

    测试通过，性能达标

--- gas-station.c
+++ gas-station.c
@@ -1,14 +1,16 @@
+#include "include_all.h"
+
 int canCompleteCircuit(const int gas_a[], int no_use, const int cost_a[],
 		       int length) {
 	int rest = 0, min_rest = 0, i = 0, min_i = 0;
 	do {
 		rest += gas_a[i] - cost_a[i];
+		i++;
 		if (rest < min_rest) {
 			min_rest = rest;
 			min_i = i;
 		}
-		i++;
 
 	} while (i != length);
-	return rest >= 0 ? min_i : -1;
+	return rest < 0 ? -1 : min_i;
 }
