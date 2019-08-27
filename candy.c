a721674f05cf601cfc7361e9e609f16f22f81de8
zzc2422 <liksasdilasely@163.com>
2019-08-27 21:36:06 +0800

    可能彻底错误，暂时存档

--- candy.c
+++ candy.c
@@ -4,12 +4,12 @@ int candy(const int rate_a[], int stu_amount) {
 	if (stu_amount == 0) {
 		return 0;
 	}
-	const int *rate_end_np = rate_a + stu_amount, *p = rate_a + 1;
+	const int *rate_end_np = rate_a + stu_amount;
 	int rate = rate_a[0], dec_length = 1, candy = 1, sum = 1;
-	while (1) {
+	for (const int *p = rate_a + 1; p != rate_end_np; p++) {
 		int new_rate = *p;
 		if (new_rate >= rate) {
-			dec_length = 1;
+			dec_length = 0;
 			if (new_rate == rate) {
 				candy = 1;
 			} else {
@@ -19,11 +19,17 @@ int candy(const int rate_a[], int stu_amount) {
 			if (candy == 1) {
 				sum += dec_length;
 			} else {
-				candy--;
+				candy = 1;
 			}
 			dec_length++;
 		}
 		sum += candy;
+		rate = new_rate;
 	}
 	return sum;
+}
+
+int main() {
+	int rate_a[] = {1, 3, 4, 5, 2}, length = sizeof(rate_a) / sizeof(int);
+	printf("%d\n", candy(rate_a, length));
 }
