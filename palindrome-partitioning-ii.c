1f9c94b7cb2f1f78515fd5a40ae4dcae088c775d
zzc2422 <liksasdilasely@163.com>
2019-08-27 15:06:31 +0800

    代码完成，但有错误

--- palindrome-partitioning-ii.c
+++ palindrome-partitioning-ii.c
@@ -1,4 +1,4 @@
-// 未完成
+﻿// 未完成
 
 #include "include_all.h"
 #include <stdio.h>
@@ -10,40 +10,64 @@ static const char *_get_end_np(const char str_a[]) {
 	return p;
 }
 
-// cut_times_a[i]表示从str_start_p[i + 1]开始之后的串分割次数
-static int _seg_amount(const char *str_start_p, const char *str_end_np,
-		       const int *pl_start_p, const int *pl_end_np,
-		       const int seg_amount_a[]) {
-	int length = str_end_np - str_start_p, seg_amount = length;
-	const int *p = pl_start_p;
+// 注意：str长度至少为2
+static const int *_pl_end_np(const char *str_start_p, const char *str_end_np,
+			     int *pl_start_p, const int *pl_end_np) {
+	char first_char = str_start_p[0];
+	const int *inp = pl_start_p;
+	int *outp = pl_start_p;
 	do {
-		int palin_len = *p, after_len = length - palin_len;
-		if (after_len <= 1) {
-			return after_len + 1;
+		int palin_len = *inp;
+		const char *palin_end_np = str_start_p + 1 + palin_len;
+		if (palin_end_np != str_end_np && *palin_end_np == first_char) {
+			*outp = palin_len + 2;
+			outp++;
 		}
-		int sai = seg_amount_a[palin_len - 1] + 1;
-		if (sai < seg_amount) {
-			seg_amount = sai;
-		}
-		p++;
-	} while (p != pl_end_np);
-	return seg_amount;
+		inp++;
+	} while (inp != pl_end_np);
+	if (str_start_p[1] == first_char) {
+		*outp = 2;
+		outp++;
+	}
+	*outp = 1;
+	return outp + 1;
 }
 
 int minCut(const char str_a[]) {
 	const char *end_np = _get_end_np(str_a);
 	if (end_np <= str_a + 1) {
-		return 1;
+		return 0;
 	}
 	int length = end_np - str_a;
-	int palin_len_a[length];
 	// cut_times_a[i]表示从str_a[i + 1]开始之后的串分割次数
-	int seg_amount_a[length - 2];
-	
+	int cut_amount_a[length], palin_len_a[length];
+	cut_amount_a[length - 1] = -1;
+	palin_len_a[0] = 1;
+	const int *pl_end_np = palin_len_a + 1;
+	for (const char *start_p = end_np - 2;; start_p--) {
+		size_t sa_offset = start_p - str_a - 1;
+		int min_cut_amount = end_np - start_p;
+		pl_end_np = _pl_end_np(start_p, end_np, palin_len_a, pl_end_np);
+		const int *p = palin_len_a;
+		do {
+			int palin_len = *p, cut_amount;
+			cut_amount = cut_amount_a[palin_len + sa_offset] + 1;
+			if (cut_amount < min_cut_amount) {
+				min_cut_amount = cut_amount;
+			}
+			p++;
+
+		} while (p != pl_end_np);
+		if (start_p == str_a) {
+			return min_cut_amount;
+		}
+		cut_amount_a[sa_offset] = min_cut_amount;
+	}
 }
 
 int main() {
-	const char str_a[] = "aba",
-		   length = sizeof(str_a) / sizeof(char) - 1;
+	const char str_a[] = "abacabaabac";
+	int min_seg_amount = minCut(str_a);
+	printf("%d\n", min_seg_amount);
 	return 0;
 }
