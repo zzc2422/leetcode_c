c1d0119fb6029f24454064dd519d24713d71a15a
zzc2422 <liksasdilasely@163.com>
2019-08-30 15:50:24 +0800

    测试通过，性能有待提高

--- insertion-sort-list.c
+++ insertion-sort-list.c
@@ -1,7 +1,8 @@
 #include "include_all.h"
 
-static struct ListNode* _insert_point(struct ListNode* head_pre_p, int number) {
-	struct ListNode* p = head_pre_p;
+static struct ListNode* _insert_point(struct ListNode* head_prev_p,
+				      int number) {
+	struct ListNode* p = head_prev_p;
 	while (1) {
 		struct ListNode* next_p = p->next;
 		int next_number = next_p->val;
@@ -12,33 +13,24 @@ static struct ListNode* _insert_point(struct ListNode* head_pre_p, int number) {
 	}
 }
 
-// 注意：new_pre_p不能比old_pre_p靠后
-static void _move(struct ListNode* new_pre_p, struct ListNode* old_pre_p) {
-	struct ListNode *to_move_p = old_pre_p->next,
-			*old_next_p = to_move_p->next;
-	old_pre_p->next = old_next_p;
-	struct ListNode* new_next_p = new_pre_p->next;
-	new_pre_p->next = to_move_p;
-	to_move_p->next = new_next_p;
-}
-
 struct ListNode* insertionSortList(struct ListNode* head_p) {
 	if (head_p == NULL) {
 		return head_p;
 	}
-	struct ListNode *p = head_p, *next_p = p->next;
-	struct ListNode head_pre;
-	head_pre.next = head_p;
-	while (next_p != NULL) {
-		int number = next_p->val;
-		struct ListNode* insert_p = _insert_point(&head_pre, number);
-		struct ListNode* next_next_p = next_p->next;
-		if (p == insert_p) {
-			p = next_p;
+	struct ListNode head_prev = {.next = head_p};
+	struct ListNode *end_p = head_p, *p = head_p->next;
+	while (p != NULL) {
+		struct ListNode* next_p = p->next;
+		struct ListNode* insert_p = _insert_point(&head_prev, p->val);
+		if (insert_p == end_p) {
+			end_p = p;
 		} else {
-			_move(insert_p, p);
+			p->next = insert_p->next;
 		}
-		next_p = next_next_p;
+		insert_p->next = p;
+		end_p->next = next_p;
+		p = next_p;
 	}
-	return head_pre.next;
+	end_p->next = NULL;
+	return head_prev.next;
 }
