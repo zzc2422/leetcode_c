64aa3d8015fd1ccfb6e3da83089dc5d095147b97
zzc2422 <liksasdilasely@163.com>
2019-09-03 13:38:02 +0800

    leetcode提示超时，原因不明

--- sort-list.c
+++ sort-list.c
@@ -1,7 +1,5 @@
 #include "include_all.h"
 
-#define POWER 32
-
 // 注意：链表两部分长度都至少为1
 static struct ListNode* _merge_sort(struct ListNode* start1_p,
 				    struct ListNode* start2_p,
@@ -32,26 +30,46 @@ static struct ListNode* _merge_sort(struct ListNode* start1_p,
 
 // 假定链表长度不超过2^POWER
 struct ListNode* sortList(struct ListNode* head_p) {
+	if (head_p == NULL) {
+		return NULL;
+	}
 	int index = 2;
-	struct ListNode* node_p = head_p;
-	struct ListNode *stack_pa[POWER], **spp = stack_pa + 1;
-	stack_pa[0] = head_p;
+	struct ListNode* node_p = head_p->next;
+	struct ListNode *stack_pa[32], **spp = stack_pa, *top_p = head_p;
+	stack_pa[0] = top_p;
 	while (node_p != NULL) {
 		struct ListNode* next_p = node_p->next;
-		*spp = next_p;
 		spp++;
+		*spp = node_p;
 		for (int tmp_index = index; (tmp_index & 1) == 0;
 		     tmp_index >>= 1) {
 			spp--;
-			struct ListNode* start_node_p = *(spp - 1);
+			struct ListNode* start_node_p = *spp;
 			node_p = _merge_sort(start_node_p, node_p, next_p);
-			*(spp - 1) = node_p;
+			*spp = node_p;
 		}
+		top_p = node_p;
 		node_p = next_p;
+		index++;
 	}
-	head_p = stack_pa[0];
-	if (spp != stack_pa + 1) {
-		head_p = _merge_sort(head_p, stack_pa[1], NULL);
+	// 至此，top_p为栈顶的值
+	while (spp != stack_pa) {
+		spp--;
+		struct ListNode* left_p = *spp;
+		top_p = _merge_sort(left_p, top_p, NULL);
 	}
-	return head_p;
+	return top_p;
+}
+
+int main() {
+	struct ListNode node1, node2, node3;
+	node1.val = 1;
+	node1.next = &node2;
+	node2.val = 7;
+	node2.next = &node3;
+	node3.val = 2;
+	node3.next = NULL;
+	struct ListNode* list_p = sortList(&node1);
+	printf("%d\n", list_p->val);
+	return 0;
 }
