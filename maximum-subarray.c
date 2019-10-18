acf9016f037d6ffb167c2c7a081b8ed379682046
zzc2422 <liksasdilasely@163.com>
2019-10-18 20:29:41 +0800

    测试通过，性能达标

--- maximum-subarray.c
+++ maximum-subarray.c
@@ -1,5 +1,19 @@
 #include "include_all.h"
 
 int maxSubArray(const int a[], int length) {
-	
+	const int* end_np = a + length;
+	int sum = a[0], max_sum = sum;
+	while (1) {
+		a++;
+		if (a == end_np) {
+			return max_sum;
+		}
+		if (sum < 0) {
+			sum = 0;
+		}
+		sum += *a;
+		if (sum > max_sum) {
+			max_sum = sum;
+		}
+	}
 }
