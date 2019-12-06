2303a383cb5f544842fb99a780f4a89292690d55
zzc2422 <liksasdilasely@163.com>
2019-12-07 00:00:08 +0800

    切换到two-sum-ii-input-array-is-sorted题目

---
+++ two-sum-ii-input-array-is-sorted.c
@@ -0,0 +1,10 @@
+const int* _find(const int* left_p, int* right_p, int to_find) {
+	
+}
+
+int* twoSum(const int a[], int length, int target, int* two) {
+	*two = 2;
+	for (const int* p = a; p != a + length; p++) {
+		int value = *p, diff = target - value;
+	}
+}
