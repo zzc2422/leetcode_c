9b16510837c56494cf51abeaace5b56c2163505b
zzc2422 <liksasdilasely@163.com>
2020-04-21 12:18:21 +0800

    测试通过，性能有待提高

--- best-time-to-buy-and-sell-stock-iv.c
+++ best-time-to-buy-and-sell-stock-iv.c
@@ -1,28 +1,77 @@
+#include <memory.h>
+
+static unsigned _max_profit(const unsigned positive_a[],
+			    const unsigned negative_a[], unsigned length,
+			    unsigned max_times) {
+	unsigned sum_a[length], sum = 0;
+	memset(sum_a, 0, sizeof(unsigned[length]));
+	if (max_times > length) {
+		max_times = length;
+	}
+	unsigned times = 0, sum_head = 0;
+	while (times != max_times) {
+		sum_head += positive_a[times];
+		unsigned cur = sum = sum_head;
+		unsigned sum_pre;
+		unsigned day = times;
+		while (1) {
+			sum_pre = sum_a[day];
+			sum_a[day] = sum;
+			day++;
+			if (day == length) {
+				break;
+			}
+			int price = cur - negative_a[day];
+			if (price < (int)sum_pre) {
+				price = sum_pre;
+			}
+			cur = price + positive_a[day];
+			if (cur > sum) {
+				sum = cur;
+			}
+		}
+		times++;
+	}
+	return sum;
+}
+
 int maxProfit(int max_times, int price_a[], int length) {
-	int space_a[length];
-	int *max_a = space_a, *min_a = space_a + length / 2;
+	if (length == 0) {
+		return 0;
+	}
+	int max, min, p_value;
 	const int *p = price_a, *endn_p = price_a + length;
-	int min = *p, index = 0;
+	max = min = *p;
+	unsigned space_a[length], index = 0;
+	unsigned *positive_a = space_a, *negative_a = space_a + (length >> 1);
 	while (1) {
 		p++;
 		if (p >= endn_p) {
 			break;
 		}
-		int p_value = *p, max;
+		p_value = *p;
 		if (p_value <= min) {
 			min = p_value;
 			continue;
 		}
-		do {
+		negative_a[index] = max - min;
+		while (1) {
 			max = p_value;
 			p++;
 			if (p == endn_p) {
 				break;
 			}
 			p_value = *p;
-		} while (p_value >= max);
-		max_a[index] = max;
-		min_a[index] = min;
+			if (p_value < max) {
+				break;
+			}
+		}
+		positive_a[index] = max - min;
+		min = p_value;
 		index++;
 	}
+	if (index == 0) {
+		return 0;
+	}
+	return _max_profit(positive_a, negative_a, index, max_times);
 }
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+int maxProfit(int max_times, int price_a[], int length);
+
 int main() {
-	
+	int price_a[] = {1, 3};
+	maxProfit(0, price_a, sizeof(price_a) / sizeof(int));
 	return 0;
 }
 /*
