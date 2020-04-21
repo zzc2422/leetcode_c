934b5413e55b7cbee911702f02a069728dcd607f
zzc2422 <liksasdilasely@163.com>
2020-04-21 18:28:56 +0800

    测试通过，性能达标

--- excel-sheet-column-number.c
+++ excel-sheet-column-number.c
@@ -1,2 +1,15 @@
+#include "include_all.h"
+
 int titleToNumber(char s[]) {
+	int sum = 0;
+	while (1) {
+		char c = *s;
+		if (c == '\0') {
+			break;
+		}
+		int value = c - 'A' + 1;
+		sum = sum * 26 + value;
+		s++;
+	}
+	return sum;
 }
