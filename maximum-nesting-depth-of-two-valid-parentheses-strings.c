c58b6af111a764db33a7506b4f3b4dbd60fdeff3
zzc2422 <liksasdilasely@163.com>
2020-04-22 22:44:23 +0800

    测试通过，性能达标

--- maximum-nesting-depth-of-two-valid-parentheses-strings.c
+++ maximum-nesting-depth-of-two-valid-parentheses-strings.c
@@ -1,4 +1,28 @@
 #include "include_all.h"
 
 int* maxDepthAfterSplit(char str_a[], int* length_p) {
+	int length = strlen(str_a);
+	*length_p = length;
+	int* result_a = malloc(sizeof(int[length]));
+	int depth0 = 0, depth1 = 0;
+	for (int i = 0; i != length; i++) {
+		if (str_a[i] == '(') {
+			if (depth0 < depth1) {
+				result_a[i] = 0;
+				depth0++;
+			} else {
+				result_a[i] = 1;
+				depth1++;
+			}
+		} else {
+			if (depth0 < depth1) {
+				result_a[i] = 1;
+				depth1--;
+			} else {
+				result_a[i] = 0;
+				depth0--;
+			}
+		}
+	}
+	return result_a;
 }
