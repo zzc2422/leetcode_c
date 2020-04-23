4c044689d73451b3ca80acba2b71c52fb3ccd1be
zzc2422 <liksasdilasely@163.com>
2020-04-23 17:00:53 +0800

    测试通过，性能达标

--- coin-lcci.c
+++ coin-lcci.c
@@ -1,3 +1,38 @@
-int waysToChange(int n){
+#include "include_all.h"
 
+int waysToChange(int n) {
+	const unsigned MOD = 1000000007;
+	n -= n % 5;
+	unsigned ta_a[5] = {0, 0, 0, 0};
+	unsigned* p = ta_a + 4;
+	unsigned maxle5 = 1, maxle10 = 1, maxle25 = 1;
+	unsigned maxle10p5 = 0, maxle10p10 = 0;
+	while (n != 0) {
+		*p = maxle25;
+		unsigned max1 = 1;
+		unsigned max5 = maxle5;
+		maxle5 = max1 + max5;
+		if (maxle5 >= MOD) {
+			maxle5 -= MOD;
+		}
+		maxle10p10 = maxle10p5;
+		maxle10p5 = maxle10;
+		unsigned max10 = maxle10p10;
+		maxle10 = maxle5 + max10;
+		if (maxle10 >= MOD) {
+			maxle10 -= MOD;
+		}
+		p++;
+		if (p >= ta_a + 5) {
+			p = ta_a;
+		}
+		unsigned totalp25 = *p;
+		unsigned max25 = totalp25;
+		maxle25 = maxle10 + max25;
+		if (maxle25 >= MOD) {
+			maxle25 -= MOD;
+		}
+		n -= 5;
+	}
+	return maxle25;
 }
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+int waysToChange(int n);
+
 int main() {
+	printf("%d\n", waysToChange(900750));
 	return 0;
 }
 /*
