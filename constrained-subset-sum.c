787e68f95efd96928fc0150200bb83521a23d554
zzc2422 <liksasdilasely@163.com>
2020-04-26 22:34:17 +0800

    测试通过，性能达标

--- constrained-subset-sum.c
+++ constrained-subset-sum.c
@@ -1,2 +1,86 @@
-int constrainedSubsetSum(int a[], int length, int k) {
+#include "include_all.h"
+
+typedef struct {
+	const int* int_p;
+	int value;
+} irpv_t;
+
+static irpv_t _get_max_start_endn(const int* start_p, const int* endn_p) {
+	const int *p = start_p, *max_p = p;
+	int max = *p;
+	while (1) {
+		p++;
+		if (p == endn_p) {
+			break;
+		}
+		int value = *p;
+		if (value > max) {
+			max = value;
+			max_p = p;
+		}
+	}
+	irpv_t irpv = {.int_p = max_p, .value = max};
+	return irpv;
 }
+
+static irpv_t _get_max(const int a[], int length) {
+	return _get_max_start_endn(a, a + length);
+}
+
+int constrainedSubsetSum(int a[], int length, int k) {
+	int win_cp_a[k + 1], win_max = 0, global_max = INT_MIN;
+	int* win_a = win_cp_a + 1;
+	const int* win_max_p = win_cp_a;
+	win_cp_a[0] = 0;
+	int *win_p = win_a, *ap = a;
+	while (1) {
+		int value = *ap, sum = win_max + value;
+		*win_p = sum;
+		if (win_p == win_max_p) {
+			irpv_t irpv = _get_max(win_cp_a, k + 1);
+			win_max = irpv.value;
+			win_max_p = irpv.int_p;
+		} else if (sum > win_max) {
+			win_max = sum;
+			win_max_p = win_p;
+		}
+		if (sum > global_max) {
+			global_max = sum;
+		}
+		ap++;
+		if (ap == a + length) {
+			break;
+		}
+		win_p++;
+		if (win_p == win_a + k) {
+			win_p = win_a;
+		}
+	}
+	return global_max;
+}
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+int constrainedSubsetSum(int a[], int length, int k);
+
 int main() {
+	int a[] = {-10,2,-10,-5,20}, length = sizeof(a) / sizeof(int);
+	printf("%d\n", constrainedSubsetSum(a, length, 1));
 	return 0;
 }
 /*
-
 
 
 
