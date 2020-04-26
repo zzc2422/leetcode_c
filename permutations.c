cefdb7d107fe20e8cf47b42b8d43e41d3ee974b7
zzc2422 <liksasdilasely@163.com>
2020-04-26 10:22:24 +0800

    暂时保存

--- permutations.c
+++ permutations.c
@@ -1,5 +1,21 @@
 #include "include_all.h"
 
-int** permute(int a[], int length, int* fact_p, int (*all_length_ap)[]) {
-	
+int** permute(int a[], int length, int* fact_p, int** all_length_ap) {
+	if (length == 0) {
+		*fact_p = 0;
+		*all_length_ap = malloc(0);
+		return malloc(0);
+	}
+	int fact_a[length], fact = fact_a[0] = 1;
+	for (int i = 1;; i++) {
+		fact *= i;
+		fact_a[i] = length;
+		if (i == length) {
+			break;
+		}
+	}
+	int** permute_pa = malloc(sizeof(int[fact]));
+	for (int i = 0; i != length; i++) {
+		permute_pa[i] = malloc(sizeof(int[length]));
+	}
 }
