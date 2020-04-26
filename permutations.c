0c06026ce8be34e0365b92ba91aa2ac9c7504fcc
zzc2422 <liksasdilasely@163.com>
2020-04-26 14:06:10 +0800

    未完成

--- permutations.c
+++ permutations.c
@@ -1,21 +1,45 @@
 #include "include_all.h"
 
-int** permute(int a[], int length, int* fact_p, int** all_length_ap) {
-	if (length == 0) {
-		*fact_p = 0;
-		*all_length_ap = malloc(0);
-		return malloc(0);
+int* findDiagonalOrder(int* row_pa[], int row_amount, int row_length_a[],
+		       int* total_p) {
+	int total = 0;
+	for (int i = 0; i != row_amount; i++) {
+		total += row_length_a[i];
 	}
-	int fact_a[length], fact = fact_a[0] = 1;
-	for (int i = 1;; i++) {
-		fact *= i;
-		fact_a[i] = length;
-		if (i == length) {
+	*total_p = total;
+	int *result_a = malloc(sizeof(int[total])), *p_out = result_a;
+	int next_row_a[row_amount + 1];
+	for (int i = row_amount;;) {
+		i--;
+		next_row_a[i + 1] = i;
+		if (i < 0) {
 			break;
 		}
 	}
-	int** permute_pa = malloc(sizeof(int[fact]));
-	for (int i = 0; i != length; i++) {
-		permute_pa[i] = malloc(sizeof(int[length]));
+	for (int sum = 0;; sum++) {
+		int prev_row = sum;
+		if (prev_row > row_amount) {
+			prev_row = row_amount;
+		}
+		int row = next_row_a[prev_row];
+		if (row < 0) {
+			break;
+		}
+		while (1) {
+			int column = sum - row;
+			int next_row = next_row_a[row];
+			if (column < row_length_a[row]) {
+				*p_out = row_pa[row][column];
+				p_out++;
+			} else {
+				next_row_a[prev_row] = next_row;
+			}
+			if (next_row < 0) {
+				break;
+			}
+			prev_row = row;
+			row = next_row;
+		}
 	}
+	return result_a;
 }
