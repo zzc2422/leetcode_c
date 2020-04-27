b204bd99c2e8dc567aecef602e3e03d51283b8d6
zzc2422 <liksasdilasely@163.com>
2020-04-27 10:49:37 +0800

    测试通过，性能达标

--- search-in-rotated-sorted-array.c
+++ search-in-rotated-sorted-array.c
@@ -1,3 +1,27 @@
 int search(int a[], int length, int target) {
-	
+	const int* start_a = a;
+	if (length == 0) {
+		return -1;
+	}
+	int left_value = a[0];
+	while (length != 1) {
+		int right_length = (length >> 1);
+		int left_length = length - right_length;
+		int right_start_value = start_a[left_length];
+		int b1 = (left_value < right_start_value);
+		int b2 = (target < left_value);
+		int b3 = (target < right_start_value);
+		if (b1 ^ b2 ^ b3) {
+			start_a += left_length;
+			left_value = right_start_value;
+			length = right_length;
+		} else {
+			length = left_length;
+		}
+	}
+	if (*start_a == target) {
+		return start_a - a;
+	} else {
+		return -1;
+	}
 }
