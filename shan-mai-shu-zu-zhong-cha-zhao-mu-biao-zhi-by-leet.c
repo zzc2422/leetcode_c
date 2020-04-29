30af5ad3d5fcdd5094e83969ca0b4fd865d64157
zzc2422 <liksasdilasely@163.com>
2020-04-30 00:04:57 +0800

    未完成

--- shan-mai-shu-zu-zhong-cha-zhao-mu-biao-zhi-by-leet.c
+++ shan-mai-shu-zu-zhong-cha-zhao-mu-biao-zhi-by-leet.c
@@ -3,5 +3,21 @@ int get(MountainArray*, int index);
 int length(MountainArray*);
 
 int findInMountainArray(int target, MountainArray* ma_p) {
-	
+	int base = 0, len = length(ma_p);
+	int left_value = get(ma_p, 0), right_value = get(ma_p, len - 1);
+	while (1) {
+		int left_length = len >> 1;
+		int m_value = get(ma_p, base + left_length);
+		int llm = left_value < m_value, mlr = m_value < right_value;
+		if (!(llm ^ mlr)) {
+			int tlm = target < m_value;
+			if (llm ^ tlm) {
+				len = left_length;
+			} else {
+				base += left_length;
+				len -= left_length;
+			}
+			continue;
+		}
+	}
 }
