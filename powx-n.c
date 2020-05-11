c7592ec019567c5f334616e77dc2aca170813dec
zzc2422 <liksasdilasely@163.com>
2020-05-11 11:27:07 +0800

    测试通过，性能达标

--- powx-n.c
+++ powx-n.c
@@ -1,2 +1,22 @@
-double myPow(double x, int n) {
+double myPow(double x, int in) {
+	unsigned n;
+	if (in < 0) {
+		x = 1 / x;
+		n = -(unsigned)in;
+	} else {
+		n = in;
+	}
+	double xi = x, result = 1;
+	while (1) {
+		unsigned bit = n & 1;
+		if (bit != 0) {
+			result *= xi;
+		}
+		if (n == bit) {
+			break;
+		}
+		n >>= 1;
+		xi *= xi;
+	}
+	return result;
 }
