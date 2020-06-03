82ef9febf63d0e293e55024ee079534d6ed6ef7b
zzc2422 <liksasdilasely@163.com>
2020-06-03 19:26:07 +0800

    测试通过，性能达标

--- remove-element.c
+++ remove-element.c
@@ -1,3 +1,42 @@
-int removeElement(int a[], int length, int value) {
-	
+int removeElement(int a[], int length, int remove_value) {
+	int *rem_start_p = a, *rem_endn_p;
+	const int* endn_p = a + length;
+	while (1) {
+		while (1) {
+			if (rem_start_p == endn_p) {
+				return endn_p - a;
+			}
+			int value = *rem_start_p;
+			if (value == remove_value) {
+				break;
+			}
+			rem_start_p++;
+		}
+		rem_endn_p = rem_start_p + 1;
+		while (1) {
+			if (rem_endn_p == endn_p) {
+				return rem_start_p - a;
+			}
+			int value = *rem_endn_p;
+			if (value != remove_value) {
+				break;
+			}
+			rem_endn_p++;
+		}
+		while (1) {
+			if (endn_p == rem_endn_p) {
+				return rem_start_p - a;
+			}
+			endn_p--;
+			int value = *endn_p;
+			if (value != remove_value) {
+				*rem_start_p = value;
+				rem_start_p++;
+				if (rem_start_p == rem_endn_p) {
+					break;
+				}
+			}
+		}
+		rem_start_p = rem_endn_p;
+	}
 }
