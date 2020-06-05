4ce11e532e126d32ab568292e88006041e584347
zzc2422 <liksasdilasely@163.com>
2020-06-06 02:16:30 +0800

    测试通过，性能达标

--- first-missing-positive.c
+++ first-missing-positive.c
@@ -1,3 +1,24 @@
+#include "include_all.h"
+
 int firstMissingPositive(int a[], int length) {
-	
+	for (int* p = a; p != a + length; p++) {
+		if (*p < 0) {
+			*p = 0;
+		}
+	}
+	for (int* p = a; p != a + length; p++) {
+		int value = *p;
+		if (value < 0) {
+			value &= INT_MAX;
+		}
+		if (value > 0 && value <= length) {
+			a[value - 1] |= INT_MIN;
+		}
+	}
+	for (int* p = a; p != a + length; p++) {
+		if (*p >= 0) {
+			return p - a + 1;
+		}
+	}
+	return length + 1;
 }
