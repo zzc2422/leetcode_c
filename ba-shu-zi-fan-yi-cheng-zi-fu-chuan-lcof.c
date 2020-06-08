94c5a3aed98333947c179f4bf2a14a2b2a4e8394
zzc2422 <liksasdilasely@163.com>
2020-06-09 01:00:55 +0800

    测试通过，性能达标

--- ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof.c
+++ ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof.c
@@ -1,3 +1,17 @@
 int translateNum(int num) {
-	
+	unsigned prev_bit = num % 10, prev_amount = 1, amount = 1;
+	while (1) {
+		num /= 10;
+		if (num == 0) {
+			break;
+		}
+		unsigned bit = num % 10;
+		unsigned amount_backup = amount;
+		if (bit == 1 || (bit == 2 && prev_bit <= 5)) {
+			amount += prev_amount;
+		}
+		prev_bit = bit;
+		prev_amount = amount_backup;
+	}
+	return amount;
 }
