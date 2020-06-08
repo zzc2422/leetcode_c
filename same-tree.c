482ad29cba4276dc0e20be42eba1695f41dfa694
zzc2422 <liksasdilasely@163.com>
2020-06-09 01:06:57 +0800

    测试通过，性能达标

--- same-tree.c
+++ same-tree.c
@@ -1,5 +1,9 @@
 #include "include_all.h"
 
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-	
+	if (p == NULL || q == NULL) {
+		return (p == q);
+	}
+	return p->val == q->val && isSameTree(p->left, q->left) &&
+	       isSameTree(p->right, q->right);
 }
