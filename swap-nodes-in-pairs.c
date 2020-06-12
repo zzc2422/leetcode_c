62705627c29c5d9b06be6798849471be81f27446
zzc2422 <liksasdilasely@163.com>
2020-06-12 11:40:28 +0800

    测试通过，性能达标

--- swap-nodes-in-pairs.c
+++ swap-nodes-in-pairs.c
@@ -1,4 +1,19 @@
+#include "include/list.h"
 #include "include_all.h"
 
 struct ListNode* swapPairs(struct ListNode* head_p) {
+	struct ListNode *node_p = head_p, **node_pp = &head_p;
+	while (node_p != NULL) {
+		struct ListNode* next_node_p = node_p->next;
+		if (next_node_p == NULL) {
+			break;
+		}
+		struct ListNode* next_next_node_p = next_node_p->next;
+		*node_pp = next_node_p;
+		next_node_p->next = node_p;
+		node_pp = &(node_p->next);
+		node_p = next_next_node_p;
+	}
+	*node_pp = node_p;
+	return head_p;
 }
