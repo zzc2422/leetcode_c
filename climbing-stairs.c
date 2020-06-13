a8c318a5abc7e9f8084edcd473b8cc78118685e2
zzc2422 <liksasdilasely@163.com>
2020-06-13 14:13:26 +0800

    测试通过，性能达标

--- climbing-stairs.c
+++ climbing-stairs.c
@@ -1,2 +1,13 @@
 int climbStairs(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	int amount = 1, prev_amount = 1, current = 1;
+	do {
+		current++;
+		int new_amount = amount + prev_amount;
+		prev_amount = amount;
+		amount = new_amount;
+	} while (current != n);
+	return amount;
 }
