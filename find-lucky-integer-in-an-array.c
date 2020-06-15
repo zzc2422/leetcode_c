7042e3103f8122987bb51524378c31998d7fd0c6
zzc2422 <liksasdilasely@163.com>
2020-06-15 09:56:44 +0800

    测试通过，性能达标

--- find-lucky-integer-in-an-array.c
+++ find-lucky-integer-in-an-array.c
@@ -1,2 +1,19 @@
+#include "include_all.h"
+
 int findLucky(int a[], int length) {
+	int times_a[length];
+	memset(times_a, 0, sizeof(times_a));
+	for (int i = 0; i != length; i++) {
+		int ai = a[i];
+		if (ai <= length) {
+			times_a[ai - 1]++;
+		}
+	}
+	for (int i = length; i != 0; i--) {
+		int times = times_a[i - 1];
+		if (times == i) {
+			return times;
+		}
+	}
+	return -1;
 }
