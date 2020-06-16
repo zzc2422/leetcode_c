d6a86ad2471cda3bd77b4799e6ab29698b517b3e
zzc2422 <liksasdilasely@163.com>
2020-06-17 01:28:51 +0800

    测试通过，性能有待提高，原因不明

--- best-sightseeing-pair.c
+++ best-sightseeing-pair.c
@@ -1,2 +1,14 @@
 int maxScoreSightseeingPair(int a[], int length) {
+	int left_select = 0, left_score = a[0], max_total = left_score;
+	for (int i = 1; i != length; i++) {
+		int score = a[i], total = left_score + score + left_select - i;
+		if (total > max_total) {
+			max_total = total;
+		}
+		if (score > left_score + left_select - i) {
+			left_select = i;
+			left_score = score;
+		}
+	}
+	return max_total;
 }
