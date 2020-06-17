202fb52554228c8be0c6231324504e4c67a47676
zzc2422 <liksasdilasely@163.com>
2020-06-18 00:10:46 +0800

    测试通过，性能达标

--- convert-sorted-array-to-binary-search-tree.c
+++ convert-sorted-array-to-binary-search-tree.c
@@ -1,4 +1,16 @@
 #include "include_all.h"
 
 struct TreeNode* sortedArrayToBST(int a[], int length) {
+	if (length == 0) {
+		return NULL;
+	}
+	int middle_index = length >> 1;
+	const struct TreeNode* left_p = sortedArrayToBST(a, middle_index);
+	const struct TreeNode* right_p = sortedArrayToBST(
+		a + middle_index + 1, length - middle_index - 1);
+	struct TreeNode* root_p = malloc(sizeof(struct TreeNode));
+	root_p->val = a[middle_index];
+	root_p->left = left_p;
+	root_p->right = right_p;
+	return root_p;
 }
