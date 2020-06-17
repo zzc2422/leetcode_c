4e1973ddfcf12461ccf14a298d99a23f8fc45ef6
zzc2422 <liksasdilasely@163.com>
2020-06-17 21:02:18 +0800

    测试未通过，怀疑测试用例错误

--- divide-two-integers.c
+++ divide-two-integers.c
@@ -1,2 +1,28 @@
 int divide(int dividend, int divisor) {
+	int sign = 0;
+	unsigned result = 0;
+	if (dividend < 0) {
+		dividend = -(unsigned)dividend;
+		sign = !sign;
+	}
+	if (divisor < 0) {
+		divisor = -(unsigned)divisor;
+		sign = !sign;
+	}
+	unsigned dividend_sub = dividend, divisor_move = divisor;
+	while (divisor_move < dividend_sub) {
+		divisor_move <<= 1;
+	}
+	while (divisor_move >= divisor) {
+		result <<= 1;
+		if (dividend_sub >= divisor_move) {
+			dividend_sub -= divisor_move;
+			result++;
+		}
+		divisor_move >>= 1;
+	}
+	if (sign) {
+		result = -result;
+	}
+	return result;
 }
