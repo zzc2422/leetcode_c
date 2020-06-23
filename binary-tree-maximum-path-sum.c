7d12e37ae487ce6eea2ea26458ca9c64e545abdd
zzc2422 <liksasdilasely@163.com>
2020-06-23 11:17:12 +0800

    测试通过，性能有待提高

--- binary-tree-maximum-path-sum.c
+++ binary-tree-maximum-path-sum.c
@@ -1,42 +1,74 @@
 #include "include_all.h"
 
-typedef struct {
-	unsigned node_amount, height;
-} tree_info_t;
-
-static tree_info_t _get_tree_info(const struct TreeNode* root_p) {
+static unsigned _get_node_amount(const struct TreeNode* root_p) {
 	if (root_p == NULL) {
-		tree_info_t tree_info = {.node_amount = 0, .height = 0};
-		return tree_info;
+		return 0;
 	}
-	tree_info_t left_info = _get_tree_info(root_p->left);
-	tree_info_t right_info = _get_tree_info(root_p->right);
-	unsigned left_node_amount = left_info.node_amount;
-	unsigned right_node_amount = right_info.node_amount;
-	unsigned left_height = left_info.height;
-	unsigned right_height = right_info.height;
-	unsigned node_amount = left_node_amount + right_node_amount + 1;
-	unsigned height =
-		(left_height >= right_height ? left_height : right_height) + 1;
-	tree_info_t tree_info = {.node_amount = node_amount, .height = height};
-	return tree_info;
+	unsigned left_node_amount = _get_node_amount(root_p->left);
+	unsigned right_node_amount = _get_node_amount(root_p->right);
+	return left_node_amount + right_node_amount + 1;
+}
+
+static struct TreeNode* _make_tree(const struct TreeNode* root_p,
+				   struct TreeNode pool_a[]) {
+	struct TreeNode* left_p = root_p->left;
+	if (left_p != NULL) {
+		left_p = _make_tree(left_p, pool_a);
+		pool_a = left_p + 1;
+	}
+	struct TreeNode* right_p = root_p->right;
+	if (right_p != NULL) {
+		right_p = _make_tree(right_p, pool_a);
+		pool_a = right_p + 1;
+	}
+	struct TreeNode* node_p = pool_a;
+	node_p->val = root_p->val;
+	node_p->left = left_p;
+	node_p->right = right_p;
+	return node_p;
 }
 
 int maxPathSum(struct TreeNode* root_p) {
-	tree_info_t tree_info = _get_tree_info(root_p);
-	unsigned node_amount = tree_info.node_amount;
-	unsigned height = tree_info.height;
-	struct TreeNode *pool_pa[node_amount], **pool_pp = pool_pa;
-	struct TreeNode *stack_pa[height], **spp = stack_pa;
-	struct TreeNode* node_p = root_p;
+	unsigned node_amount = _get_node_amount(root_p);
+	struct TreeNode pool_a[node_amount];
+	_make_tree(root_p, pool_a);
+	int max_sum_a[node_amount], max_double_sum = root_p->val, i = 0;
 	while (1) {
-		if (node_p == NULL) {
-			while (node_p == NULL) {
-				spp--;
-				struct TreeNode* parent_p = *spp;
-				node_p = parent_p->right;
+		const struct TreeNode* node_p = pool_a + i;
+		int value = node_p->val, double_sum = value;
+		const struct TreeNode* left_p = node_p->left;
+		int left_sum;
+		if (left_p == NULL) {
+			left_sum = 0;
+		} else {
+			left_sum = max_sum_a[left_p - pool_a];
+			if (left_sum > 0) {
+				double_sum += left_sum;
 			}
-			spp++;
 		}
+		const struct TreeNode* right_p = node_p->right;
+		int right_sum;
+		if (right_p == NULL) {
+			right_sum = 0;
+		} else {
+			right_sum = max_sum_a[right_p - pool_a];
+			if (right_sum > 0) {
+				double_sum += right_sum;
+			}
+		}
+		if (double_sum > max_double_sum) {
+			max_double_sum = double_sum;
+		}
+		i++;
+		if (i == node_amount) {
+			break;
+		}
+		int max_sum = (left_sum >= right_sum ? left_sum : right_sum);
+		if (max_sum < 0) {
+			max_sum = 0;
+		}
+		max_sum += value;
+		max_sum_a[i - 1] = max_sum;
 	}
+	return max_double_sum;
 }
